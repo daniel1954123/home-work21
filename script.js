4da15ecdd49e6bfcf4638d53e77bdf09105dd611
“daniel1954123” <m.danik20.07@gmail.com>
2021-09-02 15:04:46 +0600

    home-work21

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict'
+
+// const start = document.querySelector(`#start`);
+//
+// const stop = document.querySelector(`#stop`);
+//
+// let seconds;
+//
+// function startFunc (){
+//     let i = 10;
+//    seconds = setInterval(function () {
+//        document.querySelector('p').innerHTML = --i;
+//    },1000)
+// }
+// function stopFunc () {
+//     clearInterval(seconds)
+// }
+//
+// start.addEventListener('click',startFunc);
+// stop.addEventListener('click',stopFunc);
+
+// Сделайте кнопку, по нажатию на которую в консоль будет выводится обратный
+// отсчет, начиная с 100.
+
+// const start = document.querySelector('#start');
+//
+// const stop = document.querySelector('#stop');
+//
+// let stopwatch;
+//
+// function startFunc () {
+//     let i = 100;
+//     stopwatch = setInterval(function () {
+//         document.querySelector('p').innerHTML = --i
+//     },1000)
+// }
+// function stopFunc () {
+//     clearInterval(stopwatch)
+// }
+// start.addEventListener('click',startFunc);
+// stop.addEventListener('click',stopFunc);
+
+
+// function func() {
+//     console.log(this);
+// }
+//
+// func();
+
